419461b0e2f704c3328f2ec238ccf588b8e1002e
SionHu <hu440@purdue.edu>
2020-04-01 17:49:45 -0400

    add instruction on main()

--- simple_solution/main.py
+++ simple_solution/main.py
@@ -2,14 +2,20 @@ import pkgutil
 import sys
 
 def main():
-	# open video and questions files
+	"""
+		This is the script wil be used to execute your solution. Make sure your
+		ourput format is the same as instructed in the GitHub repo.
+
+		Notice that the input format should not be changed. The following command will be used
+		to run your solution (after unzip your solution.pyz):
+		$ python solution/ video.mp4 questions.txt
+	"""
 	with open(sys.argv[1]) as video, open(sys.argv[2]) as questions:
 		# load resources
 		print(pkgutil.get_data(__name__, "resource.txt").decode())
 
 		# do something...
 
-	# write answers
-	with open("usr_result.txt", 'w') as f:
+	# write answers and output to answers.txt
+	with open("answers.txt", 'w') as f:
 		print("EXIT:STAIR 1 STAR 1 ; GTR:a Bort 1 Skyline R34 ; ", file=f)
-
